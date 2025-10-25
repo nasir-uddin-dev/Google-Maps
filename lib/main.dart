a20114dc1966530be7de516fef2fd8482c6cc8d7
nasir-uddin-dev <nasiruddin3954@gmail.com>
2025-10-25 14:29:28 +0600

    google_map

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:location/location.dart';
 
 void main() {
   runApp(const MyApp());
@@ -23,18 +26,91 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Location _locationController = Location();
+
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+  static const _pGooglePlex = LatLng(37.4223, -122.0848);
+  static const _pApplePark = LatLng(37.3323, -122.0090);
+  LatLng? currentP = null;
+
+  @override
+  void initState() {
+    super.initState();
+    _getLocationUpdate();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Google Maps")),
-      body: GoogleMap(
-        mapType: MapType.hybrid,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(37.4223, -122.0848),
-          zoom: 14
-        ),
-      ),
+      body: currentP == null
+          ? Center(child: CircularProgressIndicator())
+          : GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: CameraPosition(
+                target: _pGooglePlex,
+                zoom: 13,
+              ),
+              onMapCreated: (GoogleMapController controller) {
+                _mapController.complete(controller);
+              },
+              markers: {
+                Marker(
+                  markerId: MarkerId("_currentLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pGooglePlex,
+                ),
+                Marker(
+                  markerId: MarkerId("_sourceLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pApplePark,
+                ),
+                Marker(
+                  markerId: MarkerId("_destinationLocation"),
+                  icon: BitmapDescriptor.defaultMarker,
+                  position: _pApplePark,
+                ),
+              },
+            ),
     );
+  }
+
+  Future<void> _cameraToPosition(LatLng pos) async {
+    final GoogleMapController controller = await _mapController.future;
+    CameraPosition _newCameraPositon = CameraPosition(target: pos, zoom: 13);
+  }
+
+  Future<void> _getLocationUpdate() async {
+    bool serviceEnabled;
+    PermissionStatus permissionGranted;
+
+    serviceEnabled = await _locationController.serviceEnabled();
+    if (serviceEnabled) {
+      serviceEnabled = await _locationController.requestService();
+    } else {
+      return;
+    }
+
+    permissionGranted = await _locationController.hasPermission();
+    if (permissionGranted == PermissionStatus.denied) {
+      permissionGranted = await _locationController.requestPermission();
+      if (permissionGranted != PermissionStatus.granted) {
+        return;
+      }
+    }
+
+    _locationController.onLocationChanged.listen((LocationData currentLocation) {
+      if (currentLocation.latitude != null &&
+          currentLocation.longitude != null) {
+        setState(() {
+          currentP = LatLng(
+            currentLocation.latitude!,
+            currentLocation.longitude!,
+          );
+          print(currentP);
+        });
+      }
+    });
   }
 }
